00b71fa5df1ab858747cef1a1e3d97073103f7c3
Alejandra SM <alejandrasanmartin2911@gmail.com>
2023-10-09 16:39:20 -0500

    Initial Commit

---
+++ GitHubAPI/GitHubAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAPIApp.swift
+//  GitHubAPI
+//
+//  Created by Alejandra San Martin on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
